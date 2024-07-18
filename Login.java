965fac8bfbb7e4c7ee25baeceda15f780d200cf6
alexbndr88 <alexbndr88@gmail.com>
2024-07-17 19:50:59 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -3,7 +3,7 @@ class Test
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
-        System.out.println("practicing fetch");
+        System.out.println("practicing fetch hello");
     }
     
 };
