3cb63ae837f52f0fbbe7260a8c122a6d45542a6c
Oles Makarenko <oles@MacBook-Pro.local>
2024-07-17 20:33:29 -0500

    Your commit message

--- Login.java
+++ Login.java
@@ -3,7 +3,11 @@ class Test
     public static void main(String []args)
     {
         System.out.println("My First Java Program.");
+<<<<<<< HEAD
         System.out.println("practicing fetch hello");
+=======
+
+>>>>>>> e6e589f (Your commit message)
     }
     
 };
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-documentation
+"sdad"   documentation
